9ad88adfccdd54395226808edbcdfc8491928fb8
Влад <skibav2002@gmail.com>
2023-02-09 23:53:42 +0300

    implement staging buffer case (we still need host cogerent memory type)

--- src/vulkanapp/resourceManager.rs
+++ src/vulkanapp/resourceManager.rs
@@ -15,11 +15,13 @@ pub enum HostAccessPolicy {
 pub struct Resource {
     pub buffer: vk::Buffer,
     pub memory: vk::DeviceMemory,
+    pub size: vk::DeviceSize,
 }
 
 pub struct ResourceManager {
     pub resources: Vec<Resource>,
     pub host_access_policy: HostAccessPolicy,
+    stagingBuffer: Option<Resource>,
 
     physical_device: vk::PhysicalDevice,
     device: ash::Device,
@@ -85,10 +87,14 @@ impl ResourceManager {
             device,
             queue,
             command_buffer,
+            stagingBuffer: None,
         }
     }
 
-    pub fn create_buffer(&mut self, size: vk::DeviceSize, usage: vk::BufferUsageFlags) -> Resource {
+    pub fn create_buffer(&mut self, size: vk::DeviceSize, mut usage: vk::BufferUsageFlags) -> Resource {
+        if let HostAccessPolicy::UseStaging { host_memory_type: _, device_memory_type: _ } = self.host_access_policy {
+            usage |= vk::BufferUsageFlags::TRANSFER_DST;
+        }
         let buffer_create_info = vk::BufferCreateInfo::builder()
             .size(size)
             .usage(usage)
@@ -104,8 +110,10 @@ impl ResourceManager {
                     .allocation_size(memory_requirements.size)
                     .memory_type_index(memory_type as u32)
             },
-            _ => {
-                todo!("implement staging buffer case");
+            HostAccessPolicy::UseStaging { host_memory_type, device_memory_type } => {
+                vk::MemoryAllocateInfo::builder()
+                    .allocation_size(memory_requirements.size)
+                    .memory_type_index(host_memory_type as u32)
             }
         };
 
@@ -116,6 +124,7 @@ impl ResourceManager {
         let res = Resource {
             buffer,
             memory,
+            size,
         };
         self.resources.push(res);
 
@@ -123,6 +132,10 @@ impl ResourceManager {
     }
 
     pub fn fill_buffer<T: Copy + Debug>(&mut self, resource: Resource, data: &[T]) {
+        //size check
+        let size = (data.len() * std::mem::size_of::<T>()) as vk::DeviceSize;
+        assert!(size <= resource.size);
+
         match self.host_access_policy {
             HostAccessPolicy::SingleBuffer(_) => {
                 unsafe {
@@ -132,8 +145,103 @@ impl ResourceManager {
                     self.device.unmap_memory(resource.memory);
                 }
             },
-            _ => {
-                todo!("implement staging buffer case");
+            HostAccessPolicy::UseStaging { host_memory_type, device_memory_type: _ } => {
+                let staging_buffer: Resource;
+                
+                if let Some(staging) = self.stagingBuffer.take() {
+                    staging_buffer = staging;
+                } else {
+                    let buffer_create_info = vk::BufferCreateInfo::builder()
+                        .size(size)
+                        .usage(vk::BufferUsageFlags::TRANSFER_SRC)
+                        .sharing_mode(vk::SharingMode::EXCLUSIVE);
+                    
+                    let buffer = unsafe {self.device.create_buffer(&buffer_create_info, None)}.unwrap();
+
+                    let memory_requirements = unsafe {self.device.get_buffer_memory_requirements(buffer)};
+
+                    let memory_allocate_info = vk::MemoryAllocateInfo::builder()
+                        .allocation_size(memory_requirements.size)
+                        .memory_type_index(host_memory_type as u32);
+                    
+                    let memory = unsafe {self.device.allocate_memory(&memory_allocate_info, None)}.unwrap();
+
+                    unsafe {self.device.bind_buffer_memory(buffer, memory, 0)}.unwrap();
+
+                    staging_buffer = Resource {
+                        buffer,
+                        memory,
+                        size,
+                    };
+                }
+                unsafe {
+                    let mem_ptr = self.device.map_memory(staging_buffer.memory, 0, vk::WHOLE_SIZE, vk::MemoryMapFlags::empty()).unwrap();
+                    let mem_slice = std::slice::from_raw_parts_mut(mem_ptr as *mut T, data.len());
+                    mem_slice.copy_from_slice(data);
+                    self.device.unmap_memory(staging_buffer.memory);
+                }
+
+                let copy_region = vk::BufferCopy::builder()
+                    .size((data.len() * std::mem::size_of::<T>()) as vk::DeviceSize);
+
+
+                unsafe {
+                    self.device.begin_command_buffer(self.command_buffer, 
+                        &vk::CommandBufferBeginInfo::builder()
+                        .flags(vk::CommandBufferUsageFlags::ONE_TIME_SUBMIT)).unwrap();
+                }
+
+                let buffer_memory_barrier = vk::BufferMemoryBarrier::builder()
+                    .src_access_mask(vk::AccessFlags::HOST_WRITE)
+                    .dst_access_mask(vk::AccessFlags::TRANSFER_READ)
+                    .buffer(staging_buffer.buffer)
+                    .offset(0)
+                    .size(vk::WHOLE_SIZE);
+
+
+                unsafe {
+                    self.device.cmd_pipeline_barrier(
+                        self.command_buffer,
+                        vk::PipelineStageFlags::HOST,
+                        vk::PipelineStageFlags::TRANSFER,
+                        vk::DependencyFlags::empty(),
+                        &[],
+                        &[buffer_memory_barrier.build()],
+                        &[],
+                    );
+                    self.device.cmd_copy_buffer(self.command_buffer, staging_buffer.buffer, resource.buffer, &[copy_region.build()]);
+                    
+                }
+
+                //barrier transfer write to vertex shader read
+                let buffer_memory_barrier = vk::BufferMemoryBarrier::builder()
+                    .src_access_mask(vk::AccessFlags::TRANSFER_WRITE)
+                    .dst_access_mask(vk::AccessFlags::VERTEX_ATTRIBUTE_READ)
+                    .buffer(resource.buffer)
+                    .offset(0)
+                    .size(vk::WHOLE_SIZE);
+                
+                unsafe {
+                    self.device.cmd_pipeline_barrier(
+                        self.command_buffer,
+                        vk::PipelineStageFlags::TRANSFER,
+                        vk::PipelineStageFlags::VERTEX_INPUT,
+                        vk::DependencyFlags::empty(),
+                        &[],
+                        &[buffer_memory_barrier.build()],
+                        &[],
+                    );
+
+                    self.device.end_command_buffer(self.command_buffer).unwrap();
+                }
+
+                unsafe {
+                    let submit_info = vk::SubmitInfo::builder()
+                        .command_buffers(&[self.command_buffer])
+                        .build();
+                    self.device.queue_submit(self.queue, &[submit_info], vk::Fence::null()).unwrap();
+                }
+                self.stagingBuffer = Some(staging_buffer);
             }
         }
     }
